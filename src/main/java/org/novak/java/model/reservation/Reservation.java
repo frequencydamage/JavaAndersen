535dbba657739c722bf3fd6c9a594e13e84bb886
Andrej Novak <andrei.novak@trip-arc.com>
2025-03-25 20:42:44 +0100

    Add @AllArgsConstructor annotation

--- src/main/java/org/novak/java/model/reservation/Reservation.java
+++ src/main/java/org/novak/java/model/reservation/Reservation.java
@@ -1,20 +1,16 @@
 package org.novak.java.model.reservation;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.novak.java.model.workspace.WorkspaceType;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class Reservation implements Serializable {
 
     private Integer worksSpaceId;
     private Integer reservationId;
     private WorkspaceType workspaceType;
-
-    public Reservation(Integer worksSpaceId, WorkspaceType workspaceType, Integer reservationId) {
-        this.reservationId = reservationId;
-        this.worksSpaceId = worksSpaceId;
-        this.workspaceType = workspaceType;
-    }
 }
--- src/main/java/org/novak/java/model/workspace/Workspace.java
+++ src/main/java/org/novak/java/model/workspace/Workspace.java
@@ -1,21 +1,16 @@
 package org.novak.java.model.workspace;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class Workspace implements Serializable {
 
     private Integer id;
     private Double price;
     private WorkspaceType workspaceType;
     private Boolean isAvailable;
-
-    public Workspace(Integer id, Double price, WorkspaceType workspaceType, Boolean isAvailable) {
-        this.id = id;
-        this.price = price;
-        this.workspaceType = workspaceType;
-        this.isAvailable = isAvailable;
-    }
 }
