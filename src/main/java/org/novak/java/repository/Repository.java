051afdd870416b4e9d813ec98210600be85f84ff
Andrej Novak <andrei.novak@trip-arc.com>
2025-03-30 00:23:18 +0100

    fix access modifiers

--- src/main/java/org/novak/java/repository/Repository.java
+++ src/main/java/org/novak/java/repository/Repository.java
@@ -4,15 +4,15 @@ import org.novak.java.util.databaseUtil.DBUtil;
 import org.novak.java.util.databaseUtil.DBUtilResultConsumer;
 import org.novak.java.util.databaseUtil.DBUtilStatementConsumer;
 
-abstract class Repository {
+public abstract class Repository {
 
     private final DBUtil dbUtil = DBUtil.getInstance();
 
-    protected void executeUpdate(String query, DBUtilStatementConsumer statementSetter) {
+    public void executeUpdate(String query, DBUtilStatementConsumer statementSetter) {
         dbUtil.executeUpdate(query, statementSetter);
     }
 
-    protected void executeSelect(String query, DBUtilStatementConsumer statementSetter, DBUtilResultConsumer resultProcessor) {
+    public void executeSelect(String query, DBUtilStatementConsumer statementSetter, DBUtilResultConsumer resultProcessor) {
         dbUtil.executeSelect(query, statementSetter, resultProcessor);
     }
 }
