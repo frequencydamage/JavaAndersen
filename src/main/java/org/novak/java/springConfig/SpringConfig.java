322e2ab5f15a03c9760e22c0a908af922c81fe2b
frequencydamage <andrej.novak.v@gmail.com>
2025-04-03 16:38:04 +0200

    Creating Spring Config & Beans

---
+++ src/main/java/org/novak/java/springConfig/SpringConfig.java
@@ -0,0 +1,56 @@
+package org.novak.java.springConfig;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.novak.java.util.ConfigReaderUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "org.novak.java")
+public class SpringConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(ConfigReaderUtil.getProperty("DB_URL"));
+        dataSource.setUsername(ConfigReaderUtil.getProperty("DB_USER"));
+        dataSource.setPassword(ConfigReaderUtil.getProperty("DB_PASS"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setPackagesToScan("org.novak.java");
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+        jpaProperties.put("hibernate.show_sql", "false");
+        jpaProperties.put("hibernate.format_sql", "false");
+        jpaProperties.put("hibernate.session.events.log", "WARN");
+        factoryBean.setJpaProperties(jpaProperties);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
